aa3f2ede9874d6d332ab81a2ae910015262f53e6
Grace Wang <gracewang@gracedeair.home>
2024-08-22 17:22:04 -0400

    change file name

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -15,7 +15,7 @@ ls
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
 cd dir2
-touch file1.txt file2.txt file3.txt file4.txt file5.txt 
+touch file1 file2 file3 file4 file5 
 
 # 4. Append the words "Hello world" to dir2/file3
 echo "Hello world" >> file3.txt
